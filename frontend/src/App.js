2cf4a0ba816360851bf7a04fb08a074466cdc58d
Ella Rushing <ellarushing2022@gmail.com>
2024-05-13 18:15:13 -0400

    slight adjustments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,21 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <h1> Hello World </h1>
+      <h1> Hello World! </h1>
+      <h2> Hi there </h2>
+      <h3> wassup </h3>
+      <h3> ayyyy! </h3>
+      <MyButton/>
 
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
   );
 }
 
