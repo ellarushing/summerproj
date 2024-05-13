a71fb629cf6d7ee439f48d67569a7d3e93637025
Ella Rushing <ellarushing2022@gmail.com>
2024-05-13 06:25:51 -0700

    Updates

---
+++ precache-manifest.7215bec4edeb30b5e2a05ce2e935edf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0281380982c99353d5e8b87f9bdf3d",
+    "url": "/summerproj/index.html"
+  },
+  {
+    "revision": "1ab6f45aa25affd6e545",
+    "url": "/summerproj/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/summerproj/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/summerproj/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1ab6f45aa25affd6e545",
+    "url": "/summerproj/static/js/main.f0308c51.chunk.js"
+  },
+  {
+    "revision": "a1407e68ef7551dc1082",
+    "url": "/summerproj/static/js/runtime~main.95f35cef.js"
+  }
+]);
