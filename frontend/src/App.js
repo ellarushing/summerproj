2bec0f2f1ceb6ad45ea612b7390787d18b120eb1
Ella Rushing <ellarushing2022@gmail.com>
2024-05-15 15:20:51 -0400

    autofill added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,43 +5,53 @@ import 'leaflet/dist/leaflet.css';
 import './App.css';
 import ReactGlobe from './ReactGlobe';
 import MapGL from 'react-map-gl';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
+import {AddressAutofill} from '@mapbox/search-js-react';
+
 
 const App = () => {
   const MAPBOX_TOKEN = 'pk.eyJ1IjoiZWxsYXJ1c2hpbmciLCJhIjoiY2x3Nnk3dXg5MjA0eDJrcXVsaHM3dHlpeiJ9.ncqhXz4GSoEfFMgQlBw6Ow';
   const [destinationSearch, setDestinationSearch] = useState('');
   const [homeSearch, setHomeSearch] = useState('');
 
-  const handleDestinationChange = value => {
-    setDestinationSearch(value);
+  const handleDestinationChange = event => {
+    setDestinationSearch(event.target.value);
   };
 
-  const handleHomeChange = value => {
-    setHomeSearch(value);
+  const handleHomeChange = event => {
+    setHomeSearch(event.target.value);
   };
 
   const handleSearch = () => {
     console.log('Searching for: ', destinationSearch, homeSearch);
+    if(destinationSearch) {
+      fetch(`https://api.mapbox.com/search/geocode/v6/forward?q={search_text}`)
+    }
   };
 
   return (
     <div className="App" style={{ backgroundImage: "linear-gradient(orange, lightblue)" }}>
       <h1 className="welcome">Welcome!</h1>
       <ReactGlobe/>
-      <ReactSearchBox
-        className="destinationBox"
-        placeholder="Where would you like to go?"
-        value={destinationSearch}
-        onChange = {record => handleDestinationChange(record.value)}
-      />
-      <br/>
-      <ReactSearchBox
-        className="homeBox"
-        placeholder="Where are you from?"
-        value={homeSearch}
-        onChange = {record => handleHomeChange(record.value)}
+      <AddressAutofill accessToken={MAPBOX_TOKEN}>
+        <input
+          className="destinationBox"
+          placeholder="Where would you like to go?"
+
         />
-      <h3> <button onClick={handleSearch}> Search </button></h3>
+        </AddressAutofill>
+        <br/>
+        <AddressAutofill accessToken={MAPBOX_TOKEN}>
+          <input
+            className="homeBox"
+            placeholder="Where are you from?"
+         //   value={homeSearch}
+         //   onChange = {handleHomeChange}
+            />
+          </AddressAutofill>
+
+
+      <h3> <button> Search </button></h3>
     </div>
   );
 }
