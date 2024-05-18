e6cb9df8bf562ae65404c22003884b2ccccf8573
Ella Rushing <ellarushing2022@gmail.com>
2024-05-18 11:54:18 -0400

    connects server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+
+//User clicks "create account"
+
+// prompt user to fill in forms
+
+// create user
 
 app.listen(3000, () => {
     console.log("server has started on port 3000");
